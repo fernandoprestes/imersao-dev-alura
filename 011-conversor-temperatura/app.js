329ffa7c5ffd40afda942219433820c36d712b37
fernandoprestes <fernandoprestesc@gmail.com>
2021-09-14 14:39:54 -0300

    ✨ ADD kelvin

--- 011-conversor-temperatura/app.js
+++ 011-conversor-temperatura/app.js
@@ -3,6 +3,8 @@ const btn = document.querySelector('.button')
 btn.addEventListener('click', function(event) {
   event.preventDefault();
   var temperature = document.querySelector('.celsius').value;
-  var convert = (temperature * 9/5) + 32
-  document.querySelector('.fahrenheit').textContent = convert.toFixed(1);
+  var convertF = (temperature * 9/5) + 32
+  var convertK = Number(temperature) + 273.15
+  document.querySelector('.fahrenheit').textContent = convertF.toFixed(2);
+  document.querySelector('.kelvin').textContent = convertK.toFixed(2);
 });
