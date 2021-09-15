bd74bf5c49aca5388b5b6920be453d6fbb512983
fernandoprestes <fernandoprestesc@gmail.com>
2021-09-15 13:32:59 -0300

    🎨 Improve structure

--- 011-conversor-temperatura/app.js
+++ 011-conversor-temperatura/app.js
@@ -1,10 +1,10 @@
 const btn = document.querySelector('.button')
 
-btn.addEventListener('click', function(event) {
-  event.preventDefault();
-  var temperature = document.querySelector('.celsius').value;
-  var convertF = (temperature * 9/5) + 32
+btn.addEventListener('click', function (event) {
+  event.preventDefault()
+  var temperature = document.querySelector('.celsius').value
+  var convertF = (temperature * 9) / 5 + 32
   var convertK = Number(temperature) + 273.15
-  document.querySelector('.fahrenheit').textContent = convertF.toFixed(2);
-  document.querySelector('.kelvin').textContent = convertK.toFixed(2);
-});
+  document.querySelector('.fahrenheit').textContent = convertF.toFixed(2)
+  document.querySelector('.kelvin').textContent = convertK.toFixed(2)
+})
