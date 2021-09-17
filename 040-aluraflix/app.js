b10173bccaaa8c17b6b3550e78d7f956203d1586
fernandoprestes <fernandoprestesc@gmail.com>
2021-09-17 12:05:25 -0300

    ✨ add filmes iniciais e funcao listar filmes

--- 040-aluraflix/app.js
+++ 040-aluraflix/app.js
@@ -1,55 +1,77 @@
-const urlImgDefault = "https://image.tmdb.org/t/p/w500/";
-const apiKey = '' /** insert apikey */
-const urlApi =
-  `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=pt-BR&query=`;
+const urlImgDefault = 'https://image.tmdb.org/t/p/w500/'
+const apiKey = '' /** insert your apikey */
+const urlApi = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=pt-BR&query=`
 
 const btn = document.querySelector('.button')
 const textAlert = document.querySelector('.txtAlert')
 
-var listaFilmes = [];
+let divImg = document.querySelector('.movie-list')
+
+let filmesIniciais = [
+  {
+    nome: 'Guerra nas Estrelas',
+    img: 'dw7X9YPjjAfIxKHW04V64Bb9TB0.jpg'
+  },
+  {
+    nome: 'Star Wars: A Ascensão Skywalker',
+    img: 'uLlrDUtFG2tKtDcJN6kBznlqqsp.jpg'
+  },
+  {
+    nome: 'Star Wars: Os Últimos Jedi',
+    img: '5dGufuaIG5vNcxPm8QPij5MSPeQ.jpg'
+  }
+]
+
+var listaFilmesAdicionados = []
+
+for (let i = 0; i < filmesIniciais.length; i++) {
+  listarFilmes(filmesIniciais[i].img, filmesIniciais[i].nome)
+}
 
 btn.addEventListener('click', adicionarFilme)
 
 function adicionarFilme() {
   textAlert.textContent = ''
-  let nomeFilme = document.querySelector("#filme").value;
-  buscarFilme(nomeFilme);
+  let nomeFilme = document.querySelector('#filme').value
+  buscarFilme(nomeFilme)
+}
+
+function listarFilmes(banner, titulo){
+  divImg.innerHTML += htmlImg(banner, titulo)
+  listaFilmesAdicionados.push(titulo)
 }
 
 function buscarFilme(nomeFilme) {
-  let divImg = document.querySelector(".movie-list");
-  fetch(`${urlApi}${nomeFilme.toLowerCase()}`, { method: "GET" })
+  fetch(`${urlApi}${nomeFilme.toLowerCase()}`, { method: 'GET' })
     .then(resp => resp.json())
-    .then((data) => {
+    .then(data => {
       if (data.total_results > 0) {
         if (isAdded(data.results[0].title)) {
-          textAlert.textContent = "Filme já está na lista!"
+          textAlert.textContent = 'Filme já está na lista!'
         } else {
-          listaFilmes.push(data.results[0].title);
-          divImg.innerHTML +=
-            htmlImg(urlImgDefault + data.results[0].poster_path, data.results[0].title);
-          document.querySelector("#filme").value = "";
+          listarFilmes(data.results[0].poster_path, data.results[0].title)
+          document.querySelector('#filme').value = ''
         }
       } else {
-        textAlert.textContent = "Esse filme não existe!"
+        textAlert.textContent = 'Esse filme não existe!'
       }
     })
-    .catch((error) => {
-      console.log(error);
-    });
+    .catch(error => {
+      console.log(error)
+    })
 }
 
 function htmlImg(urlFilme, urlNomeFilme) {
   return `
     <div class="movie-item">
       <a class="movie-banner" title='${urlNomeFilme}'>
-        <img src="${urlFilme}">
+        <img src="${urlImgDefault + urlFilme}" alt="banner do filme ${urlNomeFilme}">
       </a>
       <p class="movie-title">${urlNomeFilme}</p>
     </div>
-  `;
+  `
 }
 
 function isAdded(nomeFilme) {
-  return listaFilmes.includes(nomeFilme);
+  return listaFilmesAdicionados.includes(nomeFilme)
 }
