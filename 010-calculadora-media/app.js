68b38e56afc6262436c04883e3baa2630c2f7dee
fernandoprestes <fernandoprestesc@gmail.com>
2021-10-02 14:18:45 -0300

    ajustes finais

--- 010-calculadora-media/app.js
+++ 010-calculadora-media/app.js
@@ -1,9 +1,5 @@
-//console.log("Bem vindo " + nome)
-//console.log(notaFinal.toFixed(0)) >> "8"
-//console.log(notaFinal) >> 8.25
-//alert(notaFinal)
-
 const btn = document.querySelector('.button')
+const btnReset = document.querySelector('.reset')
 
 btn.addEventListener('click', function (event) {
   event.preventDefault()
@@ -19,17 +15,32 @@ btn.addEventListener('click', function (event) {
     Number(notaQuarto)
   var notaFinal = notaSomada / 4
 
-  var htmlNome = document.querySelector('.user-nome')
-  var htmlResult = document.querySelector('.user-result')
-  console.log(notaFinal)
-
-  if (notaFinal > 7) {
-    htmlNome.textContent = 'Parabéns ' + nome
-    htmlResult.textContent = 'Aprovado!'
-    htmlResult.style.color = '#0DF00E'
-  } else {
-    htmlNome.textContent = 'Não foi dessa vez ' + nome
-    htmlResult.textContent = 'Reprovado'
-    htmlResult.style.color = '#F01717'
+  if(!nome || !notaPrimeiro || !notaSegundo || !notaTerceiro || !notaQuarto){
+    alert("Preencha com todos os campos!")
+    console.log("não tem os campos preenchidos!")
+  }else{
+    var htmlNome = document.querySelector('.user-nome')
+    var htmlResult = document.querySelector('.user-result')
+  
+    if (notaFinal > 7) {
+      htmlNome.textContent = 'Parabéns ' + nome
+      htmlResult.textContent = 'Aprovado!'
+      htmlResult.style.color = '#0DF00E'
+    } else {
+      htmlNome.textContent = 'Não foi dessa vez ' + nome
+      htmlResult.textContent = 'Reprovado'
+      htmlResult.style.color = '#F01717'
+    }
   }
+})
+
+btnReset.addEventListener('click', function (event) {
+  event.preventDefault()
+  var nome = (document.querySelector('#form-name').value = '')
+  var notaPrimeiro = (document.querySelector('#form-firstGrade').value = '')
+  var notaSegundo = (document.querySelector('#form-secondGrade').value = '')
+  var notaTerceiro = (document.querySelector('#form-thirdGrade').value = '')
+  var notaQuarto = (document.querySelector('#form-fourthGrade').value = '')
+  var htmlNome = (document.querySelector('.user-nome').textContent = '')
+  var htmlResult = (document.querySelector('.user-result').textContent = '')
 })
